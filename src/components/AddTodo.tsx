0e0188746ae36d21d849a1cc3200d0fa5d093a28
Anthony Diké <anthonydike@nyu.edu>
2019-07-01 15:37:02 -0400

    Set up initial code for AddTodo.tsx and TodoItems.tsx

---
+++ src/components/AddTodo.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const AddTodo = () => {
+  return (
+    <div>
+      <h1>AddTodo</h1>
+    </div>
+  );
+};
---
+++ src/components/TodoItems.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TodoItems = () => {
+  return (
+    <div>
+      <h1>TodoItems</h1>
+    </div>
+  );
+};
