4debd92d913ef5b441a5cb93d6bb51ce3aa15f74
Anthony Diké <anthonydike@nyu.edu>
2019-07-01 16:28:04 -0400

    Added comment to describe component functions

--- src/components/TodoItems.tsx
+++ src/components/TodoItems.tsx
@@ -1,3 +1,9 @@
+/**
+ * This components takes in a string prop from AddTodo.
+ * The prop gets typechecked then its mapped out into
+ * a list with a checkbox.
+ */
+
 import React from "react";
 
 const TodoItems = () => {
