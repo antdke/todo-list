9a703e1fb2e6e617ad4e4015242516099c5825b5
Anthony Diké <anthonydike@nyu.edu>
2019-07-01 15:46:14 -0400

    Added export default to both AddTodo and TodoItems & imported TodoItems into App.tsx

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,3 +1,10 @@
+/**
+ * This component possesses an input object that
+ * allows the user to input a todo task and submit it
+ * as a string prop to a TodoItems component which then maps
+ * those items into a list.
+ */
+
 import React from "react";
 
 const AddTodo = () => {
@@ -7,3 +14,5 @@ const AddTodo = () => {
     </div>
   );
 };
+
+export default AddTodo;
